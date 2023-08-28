5bc98cf65ab67e8f884917d1df99406029f74998
Vinston Salim <vinston.salim@outlook.com>
2023-08-28 20:25:37 +0200

    Implement CRUD for entity User in UserRepository

---
+++ login-management-system/app/Repository/UserRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace VinstonSalim\Learning\PHP\MVC\Repository;
+
+use VinstonSalim\Learning\PHP\MVC\Domain\User;
+
+class UserRepository
+{
+    private \PDO $connection;
+
+    // Injection via Constructor
+    /**
+     * @param \PDO $connection
+     */
+    public function __construct(\PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function save(User $user) : User {
+        $statement = $this->connection->prepare("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+        $statement->execute([
+            $user->id, $user->name, $user->password,
+        ]);
+        // Consideration: Maybe password should not be returned?
+        return $user;
+    }
+    
+    public function findById(string $id): ?User {
+        $statement = $this->connection->prepare("SELECT id,name,password FROM users WHERE id = ?");
+        $statement->execute([$id]);
+
+        try {
+            if($row = $statement->fetch()) {
+                $user = new User();
+                $user->id = $row['id'];
+                $user->name = $row['name'];
+                $user->password = $row['password'];
+
+                return $user;
+            }
+
+            return Null;
+
+        } finally {
+            $statement->closeCursor();
+        }
+    }
+
+    public function deleteAll(): void
+    {
+        $this->connection->exec("DELETE from users");
+    }
+}
