0ea7fabf5dcefee623c57fdac98aee1750e1c12e
Vinston Salim <vinston.salim@outlook.com>
2023-08-28 20:16:19 +0200

    Implement unit test for view

---
+++ login-management-system/test/App/ViewTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace VinstonSalim\Learning\PHP\MVC\App;
+
+use PHPUnit\Framework\TestCase;
+
+class ViewTest extends TestCase
+{
+
+    public function testRender()
+    {
+        View::render('Home/index', [
+            'title' => 'PHP Login Management'
+        ]);
+
+        $this->expectOutputRegex('[PHP Login Management]');
+        $this->expectOutputRegex('[html]');
+        $this->expectOutputRegex('[body]');
+        $this->expectOutputRegex('[Login Management]');
+        $this->expectOutputRegex('[Login]');
+        $this->expectOutputRegex('[Register]');
+    }
+}
