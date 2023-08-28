6668cb998b4ccf5037da7b92a6407ad0a818c9c2
Vinston Salim <vinston.salim@outlook.com>
2023-08-28 20:05:38 +0200

    Implement View Class

---
+++ login-management-system/app/App/View.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace VinstonSalim\Learning\PHP\MVC\App;
+
+class View
+{
+
+    public static function render(string $view, $model)
+    {
+        require __DIR__ . '/../View/header.php';
+        require __DIR__ . '/../View/' . $view . '.php';
+        require __DIR__ . '/../View/footer.php';
+    }
+
+}
