21d51c2f4616f03fd83fa0f162ce2cbc089514f5
Vinston Salim <vinston.salim@outlook.com>
2023-08-28 18:15:48 +0200

    Create entry point for database

---
+++ login-management-system/database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE php_login_management;
+
+CREATE DATABASE php_login_management_test;
+
+CREATE TABLE users (
+    id VARCHAR(255) primary key,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+) engine InnoDB;
+
+CREATE TABLE sessions (
+    id VARCHAR(255) primary key,
+    user_id VARCHAR(255) NOT NULL
+) engine InnoDB;
+
+
+ALTER TABLE sessions
+ADD CONSTRAINT fk_session_user
+    FOREIGN KEY (user_id)
+        REFERENCES users(id);
