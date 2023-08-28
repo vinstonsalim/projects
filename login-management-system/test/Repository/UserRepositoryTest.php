927eb44f24efaf552cf6d838c89a6b634f2e6230
Vinston Salim <vinston.salim@outlook.com>
2023-08-28 20:36:33 +0200

    Implement test

--- login-management-system/test/Repository/UserRepositoryTest.php
+++ login-management-system/test/Repository/UserRepositoryTest.php
@@ -3,8 +3,39 @@
 namespace VinstonSalim\Learning\PHP\MVC\Repository;
 
 use PHPUnit\Framework\TestCase;
+use VinstonSalim\Learning\PHP\MVC\Config\Database;
+use VinstonSalim\Learning\PHP\MVC\Domain\User;
 
 class UserRepositoryTest extends TestCase
 {
 
+    private UserRepository $userRepository;
+
+    protected function setUp(): void
+    {
+        $this->userRepository = new UserRepository(Database::getConnection());
+        $this->userRepository->deleteAll(); // Making sure the database empty before test
+    }
+
+    public function testSaveSuccess()
+    {
+        $user = new User();
+        $user->id = "Vin";
+        $user->name = "Vinston";
+        $user->password = "GanzGeheim";
+
+        $this->userRepository->save($user);
+
+        $queryResult = $this->userRepository->findById($user->id);
+
+        self::assertEquals($user->id, $queryResult->id);
+        self::assertEquals($user->name, $queryResult->name);
+        self::assertEquals($user->password, $queryResult->password);
+    }
+
+    public function testFindByIdNotFound()
+    {
+        $queryResult = $this->userRepository->findById("notFound");
+        self::assertNull($queryResult);
+    }
 }
