f373b7aa8c3921174fdf736e128aefcb220352c0
Vinston Salim <vinston.salim@outlook.com>
2023-08-28 18:35:14 +0200

    Implement test for database connection

--- login-management-system/test/Config/DatabaseTest.php
+++ login-management-system/test/Config/DatabaseTest.php
@@ -7,4 +7,16 @@ use PHPUnit\Framework\TestCase;
 class DatabaseTest extends TestCase
 {
 
+    public function testGetConnection()
+    {
+        $connection = Database::getConnection();
+        self::assertNotNull($connection);
+    }
+
+    public function testGetConnectionSingleton()
+    {
+        $connection1 = Database::getConnection();
+        $connection2 = Database::getConnection();
+        self::assertSame($connection1, $connection2);
+    }
 }
