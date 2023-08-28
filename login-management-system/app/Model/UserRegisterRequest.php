5d5ab2b4de8e4a0888f9cd330b2f654f18392fe0
Vinston Salim <vinston.salim@outlook.com>
2023-08-28 21:18:15 +0200

    Model for registration response and request

---
+++ login-management-system/app/Model/UserRegisterRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VinstonSalim\Learning\PHP\MVC\Model;
+
+class UserRegisterRequest
+{
+    public ?string $id = null;
+    public ?string $name = null;
+    public ?string $password = null;
+
+}
---
+++ login-management-system/app/Model/UserRegisterResponse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace VinstonSalim\Learning\PHP\MVC\Model;
+
+use VinstonSalim\Learning\PHP\MVC\Domain\User;
+
+class UserRegisterResponse
+{
+    public User $user;
+}
