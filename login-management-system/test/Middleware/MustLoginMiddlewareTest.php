d2000db362eb98a107f1e09cbb6e02f86c25fe34
Vinston Salim <vinston.salim@outlook.com>
2023-09-20 19:11:01 +0200

    Fix bug in Session Middleware Test

--- login-management-system/test/Middleware/MustLoginMiddlewareTest.php
+++ login-management-system/test/Middleware/MustLoginMiddlewareTest.php
@@ -12,15 +12,29 @@ namespace VinstonSalim\Learning\PHP\MVC\App {
 namespace VinstonSalim\Learning\PHP\MVC\Middleware {
 
     use PHPUnit\Framework\TestCase;
+    use VinstonSalim\Learning\PHP\MVC\Config\Database;
+    use VinstonSalim\Learning\PHP\MVC\Domain\User;
+    use VinstonSalim\Learning\PHP\MVC\Domain\Session;
+    use VinstonSalim\Learning\PHP\MVC\Repository\SessionRepository;
+    use VinstonSalim\Learning\PHP\MVC\Repository\UserRepository;
+    use VinstonSalim\Learning\PHP\MVC\Service\SessionService;
 
     class MustLoginMiddlewareTest extends TestCase
     {
         private MustLoginMiddleware $mustLoginMiddleware;
+        private UserRepository $userRepository;
+        private SessionRepository $sessionRepository;
 
         protected function setUp(): void
         {
             $this->mustLoginMiddleware = new MustLoginMiddleware();
             putenv("mode=test");
+
+            $this->userRepository = new UserRepository(Database::getConnection());
+            $this->sessionRepository = new SessionRepository(Database::getConnection());
+
+            $this->userRepository->deleteAll();
+            $this->sessionRepository->deleteAll();
         }
 
         public function testBeforeGuest(): void
@@ -30,13 +44,26 @@ namespace VinstonSalim\Learning\PHP\MVC\Middleware {
             $this->expectOutputRegex("[Location: /users/login]");
         }
 
-        public function testBeforeLoggenUser(): void
+        public function testBeforeLoggedUser(): void
         {
+            $user = new User();
+            $user->id = "testId";
+            $user->name = "testName";
+            $user->password = password_hash("testPassword", PASSWORD_BCRYPT);
+
+            $this->userRepository->save($user);
+
+            $session = new Session();
+            $session->id = uniqid();
+            $session->userId = $user->id;
+
+            $this->sessionRepository->save($session);
+
+            $_COOKIE[SessionService::$COOKIE_NAME] = $session->id;
 
             $this->mustLoginMiddleware->before();
             $this->expectOutputString("");
         }
-
     }
 
 }
--- login-management-system/test/Middleware/MustNotLoginMiddlewareTest.php
+++ login-management-system/test/Middleware/MustNotLoginMiddlewareTest.php
@@ -1,10 +1,70 @@
 <?php
 
-namespace VinstonSalim\Learning\PHP\MVC\Middleware;
+namespace VinstonSalim\Learning\PHP\MVC\App {
 
-use PHPUnit\Framework\TestCase;
-
-class MustNotLoginMiddlewareTest extends TestCase
-{
-
+    function header(string $value): void
+    {
+        echo "$value";
+    }
 }
+
+
+namespace VinstonSalim\Learning\PHP\MVC\Middleware{
+
+    use PHPUnit\Framework\TestCase;
+    use VinstonSalim\Learning\PHP\MVC\Config\Database;
+    use VinstonSalim\Learning\PHP\MVC\Domain\Session;
+    use VinstonSalim\Learning\PHP\MVC\Domain\User;
+    use VinstonSalim\Learning\PHP\MVC\Repository\SessionRepository;
+    use VinstonSalim\Learning\PHP\MVC\Repository\UserRepository;
+    use VinstonSalim\Learning\PHP\MVC\Service\SessionService;
+
+    class MustNotLoginMiddlewareTest extends TestCase
+    {
+        private MustNotLoginMiddleware $mustNotLoginMiddleware;
+        private UserRepository $userRepository;
+        private SessionRepository $sessionRepository;
+
+        protected function setUp(): void
+        {
+            $this->mustNotLoginMiddleware = new MustNotLoginMiddleware();
+            putenv("mode=test");
+
+            $this->userRepository = new UserRepository(Database::getConnection());
+            $this->sessionRepository = new SessionRepository(Database::getConnection());
+
+            $this->userRepository->deleteAll();
+            $this->sessionRepository->deleteAll();
+        }
+
+        public function testBeforeGuest(): void
+        {
+            $this->mustNotLoginMiddleware->before();
+            $this->expectOutputString("");
+
+        }
+
+        public function testBeforeLoggedUser(): void
+        {
+            $user = new User();
+            $user->id = "testId";
+            $user->name = "testName";
+            $user->password = password_hash("testPassword", PASSWORD_BCRYPT);
+
+            $this->userRepository->save($user);
+
+            $session = new Session();
+            $session->id = uniqid();
+            $session->userId = $user->id;
+
+            $this->sessionRepository->save($session);
+
+            $_COOKIE[SessionService::$COOKIE_NAME] = $session->id;
+
+            $this->mustNotLoginMiddleware->before();
+            $this->expectOutputRegex("[Location: /]");
+
+        }
+    }
+}
+
