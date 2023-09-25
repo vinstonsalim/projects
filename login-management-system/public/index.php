3f968359a450e643f490c8fbcfa6b60466918dc6
Vinston Salim <vinston.salim@outlook.com>
2023-09-25 22:04:10 +0200

    Update Template

--- login-management-system/public/index.php
+++ login-management-system/public/index.php
@@ -20,6 +20,8 @@ Router::add('POST', '/users/login', UserController::class, 'postLogin',[MustNotL
 Router::add('GET', '/users/logout', UserController::class, 'logout',[MustLoginMiddleware::class]);
 Router::add('GET', '/users/profile', UserController::class, 'updateProfile',[MustLoginMiddleware::class]);
 Router::add('POST', '/users/profile', UserController::class, 'postUpdateProfile',[MustLoginMiddleware::class]);
+Router::add('GET', '/users/password', UserController::class, 'updatePassword',[MustLoginMiddleware::class]);
+Router::add('POST', '/users/password', UserController::class, 'postUpdatePassword',[MustLoginMiddleware::class]);
 
 
 Router::run();
